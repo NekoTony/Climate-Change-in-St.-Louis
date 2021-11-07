0a146f7f10b3bfbefe70cfbfcba9fd731b495f40
Antonio Vallez <arvallez113@students.ecsu.edu>
2021-11-07 18:30:40 -0500

    s

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ def hello():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True, port=3209)
+    app.run(host='127.0.0.1', port=8080, debug=True)
